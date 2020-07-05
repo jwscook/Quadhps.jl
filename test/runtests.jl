697bb21b2628284baf23079230b08cb7d1400a90
James Cook <cookjws@gmail.com>
2020-07-05 09:04:06 +0100

    better testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,9 @@
 using Quadhps
-using ForwardDiff, SpecialFunctions, QuadGK, Test
+using ForwardDiff, QuadGK, Test
 
 @testset "Quadhps tests" begin
-  @testset "Versus QuadGK" begin
-    f(x) = 1 ./ (1 .+ x.^3)
-    lower = 0.1
-    upper = 10.0
-    a = Quadhps.quadhp(x->f(x), lower, upper)[1]
-    b = QuadGK.quadgk(x->f(x), lower, upper)[1]
-    @test a ≈ b atol=2 * eps()
-  end
 
-  @testset "Types" begin
-    f(x) = 1 ./ (1 .+ x.^3)
-    lower = BigFloat(0.1)
-    upper = 10.0
-    a = Quadhps.quadhp(x->f(x), lower, upper)[1]
-    b = QuadGK.quadgk(x->f(x), lower, upper)[1]
-    @test a ≈ b atol=2 * eps()
-  end
-
-  @testset "Accuracy" begin
+  @testset "Basic" begin
     a = Quadhps.quadhp(x->1.0, 0.0, 2.0, rtol=2*eps())
     @test a[1] ≈ 2.0 atol=10 * eps()
     @test a[2] ≈ 0.0 atol=10 * eps()
@@ -32,11 +15,31 @@ using ForwardDiff, SpecialFunctions, QuadGK, Test
     @test c[2] ≈ 0.0 atol=1000 * eps()
   end
 
+  @testset "Versus QuadGK" begin
+    f(x) = 1 ./ (1 .- x.^2)
+    lower = -0.999
+    upper = 0.999
+    a = Quadhps.quadhp(x->f(x), lower, upper, rtol=2*eps())[1]
+    b = QuadGK.quadgk(x->f(x), lower, upper, rtol=2*eps())[1]
+    @test a ≈ b atol=1000 * eps()
+  end
+
+  @testset "Types" begin
+    f(x) = x
+    lower = BigFloat(0.1)
+    upper = 10.0
+    a = Quadhps.quadhp(x->f(x), lower, upper)[1]
+    b = QuadGK.quadgk(x->f(x), lower, upper)[1]
+    @test a ≈ b rtol=eps()
+  end
+
+  @testset "Vectorised" begin
+    a = Quadhps.quadhp(x->[1, 2], 0.0, 1.0)[1]
+    @test a[1] ≈ 1 atol=2 * eps()
+    @test a[2] ≈ 2 atol=2 * eps()
+  end
+
   @testset "Intervals" begin
-    g(x::Float64) = exp(-x^2)*besselj(1, x)*besselj(-1, x) * x^4
-    g(x::Vector{T}) where {T} = g.(x)
- 
-    Quadhps.quadhp(x->[1, 2]*x, 0.0, 1.0)
     a = Quadhps.quadhp(x->3 .* x.^2 .+ 1.0, 0.0, 5.0, rtol=120*eps())
     b = Quadhps.quadhp(x->3 .* x.^2 .+ 1.0, 0.0, 2.0, 5.0, rtol=120*eps())
     c = Quadhps.quadhp(x->3 .* x.^2 .+ 1.0, 0.0, 2.0, 4.0, 5.0, rtol=120*eps())
